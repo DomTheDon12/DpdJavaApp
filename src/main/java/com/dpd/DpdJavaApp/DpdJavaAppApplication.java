1d2b0bf7be385c59d2634eda0b782052ebb4ef61
Dominik Bizony <bdomi98@gmail.com>
2023-07-28 16:37:39 +0200

    Skeleton application

---
+++ src/main/java/com/dpd/DpdJavaApp/DpdJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.dpd.DpdJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpdJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpdJavaAppApplication.class, args);
+	}
+
+}
