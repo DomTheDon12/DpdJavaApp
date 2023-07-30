fd3fba45fb2a1a4bd300933f3e3f8c9e5ae58980
Dominik Bizony <bdomi98@gmail.com>
2023-07-30 14:42:38 +0200

    Remove unused import

--- src/main/java/com/dpd/DpdJavaApp/repository/AddressRepository.java
+++ src/main/java/com/dpd/DpdJavaApp/repository/AddressRepository.java
@@ -3,7 +3,6 @@ package com.dpd.DpdJavaApp.repository;
 import com.dpd.DpdJavaApp.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
