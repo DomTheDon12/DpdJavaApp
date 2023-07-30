e86539f4ec429a03e329c6a96087b36f526adde0
Dominik Bizony <bdomi98@gmail.com>
2023-07-30 14:40:04 +0200

    Adding custom exception

---
+++ src/main/java/com/dpd/DpdJavaApp/exception/FieldValidationException.java
@@ -0,0 +1,7 @@
+package com.dpd.DpdJavaApp.exception;
+
+public class FieldValidationException extends RuntimeException {
+    public FieldValidationException(String message) {
+        super(message);
+    }
+}
